6fe4f95b1f345a810445e2b2a9a653a8371fc18f
Subiksha <124233005+Subiksha1705@users.noreply.github.com>
2025-02-05 11:36:23 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u Subiksha1705 -p SubikshA17
+    docker login -u Subiksha17 -p s1705sha17
     docker tag test Subiksha1705/Guvi_Task2
     docker push Subiksha1705/Guvi_Task2
     
