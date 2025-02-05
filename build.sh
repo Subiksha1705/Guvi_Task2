2dfacebe90036724a922c6fafd20a872878a6d32
Subiksha <124233005+Subiksha1705@users.noreply.github.com>
2025-02-05 08:51:36 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 docker build -t taskmates .
-echo Hello from subiksha
-docker login -u Subiksha1705 -p SubikshA17
-docker tag  test subi-nginx Subiksha1705/Guvi_task2:subi-nginx
-docker push Subiksha1705/Guvi_task2:subi-nginx
